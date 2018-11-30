9d998be5df996e30608cba54b48b9d715df04a5d
Stav Eyal <staveyal210@gmail.com>
2018-11-30 16:31:56 +0200

    Started working on database structure

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
 const express = require('express')
 const path = require('path')
-const Sequelize = require('sequelize')
+const model = require('./db/model.js')
 
 const app = express()
 const port = 3000
-const connection = new Sequelize('kassandra', 'root', 'password', {
-  host:'localhost',
-  dialect: 'mysql'
-})
-
-connection
-  .authenticate()
-  .then(() => {
-    console.log('Connection established')
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err)
-  })
-
-const Data = connection.define('team', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  
-  match: Sequelize.STRING,
-  team: Sequelize.INTEGER,
-
-})
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.send('Kassandra'))
 
 app.listen(port, () => console.log(`Kassandra listening on port ${port}!`))
+
---
+++ db/model.js
@@ -0,0 +1,46 @@
+const Sequelize = require('sequelize')
+const schema = 'test'
+const user = 'root'
+const password = 'password'
+// Connecting to the database
+const db = new Sequelize(schema, user, password, {
+  host:'localhost',
+  dialect: 'mysql'
+})
+// Checking the connection
+db.authenticate()
+  .then(() => {
+    console.log('Connection established')
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err)
+  })
+// Defining tables
+const Teams = db.define('teams', {
+  team_number: {
+    type: Sequelize.INT,
+    primaryKey: true,
+    allowNull: false
+  }
+}, {timestemps: false})
+
+const Events = db.define('events', {
+  event_name: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    allowNull: false
+  }
+}, {timestemps: false})
+// Insert entries
+db
+  .sync({logging: console.log})
+  .then(() => Teams.create({
+    team_number: 3316
+}))
+
+module.exports{
+  Events,
+  Teams,
+  db
+}
+
