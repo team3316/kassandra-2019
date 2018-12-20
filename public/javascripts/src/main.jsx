a61581b6a2664d04b75dfa847b408ddc3b980a23
Stav Eyal <staveyal210@gmail.com>
2018-12-20 19:20:29 +0200

    Added RankingsHeader component

--- public/javascripts/src/main.jsx
+++ public/javascripts/src/main.jsx
@@ -1,8 +1,6 @@
-/**
- * 
- */
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 /**
  * NBA is an array of objects, each objects contains the NBA team name, their wins number and theirs losses number
  * @type {Array}
@@ -18,22 +16,36 @@ const nba = [{
 }]
 
 /**
- * The RankingsTable component contains the table headers and the rows. Each row is a RankingsRow component
+ * The RankingsHeader component represents the table header, has an
+ */
+class RankingsHeader extends React.Component {
+  render () {
+    const title = this.props.title
+    console.log(title + ' rendered')
+    return (
+      <th onClick={title => this.props.handleClick(title)}> {title} </th>
+    )
+  }
+}
+
+/**
+ * The RankingsTable component contains the table headers and the rows
+ * Each header is a RankingsHeader component and each row is a RankingsRow component
  */
 class RankingsTable extends React.Component {
   render () {
+    const headerTitles = ['Team', 'W', 'L', 'W%']
+
+    const headers = headerTitles.map((title, index) => <RankingsHeader
+      title={title}
+      key={index}
+      handleClick={title => this.props.handleClick(title)} />)
     const rows = this.props.teams.map((team, index) => <RankingsRow team={team} key={index} />)
 
     return (
       <table>
-        <thead>
-          <th> Team </th>
-          <th> W </th>
-          <th> L </th>
-          <th> W% </th>
-        </thead>
-
-        <tbody>{rows}</tbody>
+        <thead> {headers} </thead>
+        <tbody> {rows} </tbody>
       </table>
     )
   }
@@ -48,7 +60,7 @@ class RankingsRow extends React.Component {
         <td> {team.Name} </td>
         <td> {team.W} </td>
         <td> {team.L} </td>
-        <td> {100 * team.W / (team.W + team.L) } </td>
+        <td> {team.WPercent} </td>
       </tr>
     )
   }
@@ -70,18 +82,42 @@ class FilterableRankingTable extends React.Component {
       value: ''
     }
 
-    this.handleChange = this.handleChange.bind(this)
+    this.standings = this.props.teams.map(function (team) {
+      team.WPercent = 100 * team.W / (team.W + team.L)
+      return team
+    })
   }
 
-  handleChange (event) {
-    this.setState({ value: event.target.value })
+  // this.handleChange = this.handleChange.bind(this)
+
+  // handleChange (event) {
+  //   this.setState({ value: event.target.value })
+  // }
+
+  sortBy (array, key) {
+    return array.sort((a, b) => b[key] - a[key])
+  }
+
+  handleClick (title, array) {
+    console.log(title + ' clicked')
+    return this.sortBy(array, title)
+  }
+
+  addWinPercent (team) {
+    team.WPercent = 100 * team.W / (team.W + team.L)
+    return team
   }
 
   render () {
+    // .setState({ teams: teams.map(team => this.addWinPercent(team)) })
+    console.log(this.sortBy(this.standings, 'WPercent'))
     return (
       <div>
         <SearchBar />
-        <RankingsTable teams={this.props.teams} />
+        <RankingsTable
+          teams={this.standings}
+          handleClick={title => this.handleClick(title, this.standings)}
+        />
       </div>
     )
   }
