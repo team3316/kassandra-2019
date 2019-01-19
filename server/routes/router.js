5c7185ab806a44bfad77eb7c06d05f2f794d5c31
Stav Eyal <staveyal210@gmail.com>
2019-01-19 13:17:52 +0200

    Fixed routes.js conflicts

--- server/routes/router.js
+++ server/routes/router.js
@@ -5,35 +5,14 @@ const router = new Router()
 const {
   addTeam,
   homePage,
-<<<<<<< HEAD
-<<<<<<< HEAD
-  addEvent
-=======
   addEvent,
   addCycle
->>>>>>> match-list
-=======
-  addEvent,
-  addCycle
->>>>>>> 6ac2331a15fe73cfc5b2401659ad9bb7891da904
 } = require('./routes')
 
+router.post('/api/cycle', addCycle)
+router.post('/api/event', addEvent)
+router.post('/api/team', addTeam)
+
 router.get('/*', homePage)
-
-router.post('/team', addTeam)
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-router.post('/event', addEvent)
-=======
-router.post('/api/event', addEvent)
-
-router.post('/api/cycle', addCycle)
->>>>>>> match-list
-=======
-router.post('/api/event', addEvent)
-
-router.post('/api/cycle', addCycle)
->>>>>>> 6ac2331a15fe73cfc5b2401659ad9bb7891da904
 
 module.exports = router
