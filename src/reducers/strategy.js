468093c89c78c069d9415accdf8d2eeb96f78a10
Stav Eyal <staveyal210@gmail.com>
2019-02-28 12:41:28 +0200

    Fixed reducer

--- src/reducers/strategy.js
+++ src/reducers/strategy.js
@@ -24,7 +24,7 @@ export default (state = strategy, action) => {
 
       return {
         isFetchingRecords: false,
-        matches: action.matches,
+        matches: action.records,
         events
       }
 
