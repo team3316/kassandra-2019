4aea0fd0d8e90a4799e4cbf02536ae6b1b5c4636
Stav Eyal <staveyal210@gmail.com>
2019-03-04 21:59:36 +0200

    Added graph labels

--- src/views/strategy/Graphs.jsx
+++ src/views/strategy/Graphs.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { GameObjectGraph, Footer } from 'components'
 import { ComboBox, Button } from 'carbon-components-react'
+import {
+  GiPlainCircle as Cargo,
+  GiCompactDisc as Panel
+} from 'react-icons/gi'
 
 class TeamData extends Component {
   render () {
@@ -33,12 +37,14 @@ class TeamData extends Component {
         {
           !isFetchingRecords && matches.length !== 0 && team != null
             ? <div>
+              <h1> <Cargo /> Cargo </h1>
               <GameObjectGraph
                 matches={matches}
                 gameObject={'cargo'}
                 height={200}
                 width={400}
               />
+              <h1> <Panel /> Panels </h1>
               <GameObjectGraph
                 matches={matches}
                 gameObject={'panels'}
